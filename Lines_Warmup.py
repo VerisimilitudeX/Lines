de3cecc12378ca0afbeecfdeabc02a32f39d5f91
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-01 18:49:22 -0800

    Add files via upload

---
+++ Lines_Warmup.py
@@ -0,0 +1,12 @@
+import random
+random1 = random.randint(0, 300)
+random2 = random.randint(0, 300)
+print("The first number is " + str(random1))
+print("The second number is " + str(random2))
+
+if random1 > random2:
+    print(str(random1) + " is larger than " + str(random2))
+elif random2 > random1:
+    print(str(random2) + " is larger than " + str(random1))
+else:
+    print("Both numbers were the same!")
