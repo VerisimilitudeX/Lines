efc16ffd403d49747706bb9c41c29ba138b27bd5
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-07 17:29:46 -0800

    Update Digital_Signature.py

--- Digital_Signature.py
+++ Digital_Signature.py
@@ -1,8 +1,3 @@
-"""
-LESSON: 3.1 - Lines
-EXERCISE: Digital Signature
-"""
-
 #### ---- SETUP ---- ####
 
 # Import the PYGAME library
@@ -29,7 +24,6 @@ black = (0, 0, 0)
 # use window, black, and coordinates that you determine.
 
 # First Letter
-# ---> TEST AFTER EACH LINE <--- #
 top_left = (30, 0)
 bottom_left = (30, 300)
 middle_right = (150, 100)
@@ -39,7 +33,6 @@ pygame.draw.line(window, black, top_left, middle_right, 2)
 pygame.draw.line(window, black, middle_left, middle_right, 2)
 
 # Second Letter
-# ---> TEST AFTER EACH LINE <--- #
 bottom_lefta = (250, 300)
 top_middle = (300, 10)
 bottom_right = (450, 300)
@@ -55,9 +48,4 @@ pygame.draw.line(window, black, middle_middle1, middle_middle2, 2)
 pygame.display.flip()
 
 # Get input to pause the program
-# ---> TEST AFTER THIS LINE <--- #
 input()
-
-
-# Turn in your Coding Exercise.
-
