6e38b4602e67a5aa7e777d64cc326009c376dd4a
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-17 13:24:23 -0800

    Update Line.py

--- Line.py
+++ Line.py
@@ -5,7 +5,6 @@ window = pygame.display.set_mode([400, 400])
 window.fill((255, 255, 255))
 black = (0, 0, 0)
 
-# Draw lines here
 top_left = (0, 0)
 bottom_right = (400, 400)
 top_right = (0, 400)
@@ -13,7 +12,5 @@ bottom_left = (400, 0)
 pygame.draw.line(window, black, top_left, bottom_right, 2)
 pygame.draw.line(window, black, top_right, bottom_left, 2)
 
-
 pygame.display.flip()
 input("Press enter to close window")
-
